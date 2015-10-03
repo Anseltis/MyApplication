8e72264dd766a2c02a386e398334c23ce4be0e5a
Ansi <the-ansi@yandex.ru>
2015-10-04 00:41:28 +0300

    jkl

--- app/src/main/java/com/example/testcompany/myapplication/MainActivity.java
+++ app/src/main/java/com/example/testcompany/myapplication/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         //double u = subtraction.op(6,4);
         TextView textView = (TextView)findViewById(R.id.text);
-        textView.setText("hix");
+        textView.setText("hix12");
     }
 
     @Override
